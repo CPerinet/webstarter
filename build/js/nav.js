6d5779e23cfbbe80b1fb2fe2a84d2b25c6213070
cperinet <charles.perinet@gmail.com>
2018-02-23 19:58:27 +0100

    ic

---
+++ build/js/nav.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$('#toggle-container').click(function () {
+    $("#nav_content").toggleClass('open');
+    $("#toggle").toggleClass('open');
+});
+
+//# sourceMappingURL=nav.js.map
